1982d70973f5172eb6147194e0f8e7d9bb4dd657
daknya_bam <daknyabam18@gmil.com>
2021-10-16 19:30:09 +0530

    post

---
+++ my.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+// js for menu//
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.link');
+
+    
+ 
+     burger.addEventListener('click',()=>{
+       nav.classList.toggle('nav-active');
+
+       burger.classList.toggle('toggle');
+     });
+
+     
+    
+  }
+  navSlide();
